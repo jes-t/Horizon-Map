4c3b0fd0a869f2102e2a054bd1978199b14782a8
Jes T <timofeeva.zhenya.1991@gmail.com>
2023-04-09 18:37:48 +0300

    Add helpers classNames

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import "./styles/index.scss";
 import { MapAsync } from "./pages/Map/Map.async";
 import { WelcomeAsync } from "./pages/Welcome/Welcome.async";
 import { UseTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 function App() {
   const { theme, toggleTheme } = UseTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Сменить тему</button>
       <Suspense fallback={<span>Loading...</span>}>
         <Routes>
